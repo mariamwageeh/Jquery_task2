573f5afa5fdba422748c1508d82834f933721f71
mariamwageeh <99591469+mariamwageeh@users.noreply.github.com>
2024-05-22 23:12:43 +0300

    Add files via upload

---
+++ jquery_task2/js/js.js
@@ -0,0 +1,25 @@
+$('.sub1').click(function()
+{
+    $('.main').attr('src','images/img1.jpg');
+}
+);
+
+$('.sub2').click(function()
+{
+    $('.main').attr('src','images/NEWS_140529700_AR_0_DOOHTLOEJEML.webp');
+}
+);
+
+
+
+$('.sub3').click(function()
+{
+    $('.main').attr('src','images/mo2.jpeg');
+}
+);
+
+$('.sub4').click(function()
+{
+    $('.main').attr('src','images/philippians-1-23-24 (1).png');
+}
+);
